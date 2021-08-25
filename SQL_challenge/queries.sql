d125a08103a7f017528bb4bd08f63e7fdc6b9747
Negin Djalali <negindjalali@Negins-Air.lan>
2021-08-24 17:06:04 -0700

    SQL homework add

---
+++ SQL_challenge/queries.sql
@@ -0,0 +1,96 @@
+-- Select the employee number, last name, first name, sex, and salary of all employees, ordered by employee number
+SELECT
+e.emp_no,
+e.last_name,
+e.first_name,
+e.sex,
+s.salary
+FROM
+employees e
+INNER JOIN salaries s ON
+e.emp_no = s.emp_no
+ORDER BY emp_no
+
+-- Select the first name, last name, and hiring date for all employees hired in 1986 
+SELECT * FROM employees
+
+SELECT
+first_name,
+last_name,
+hire_date
+FROM
+employees
+where extract(year from hire_date) = 1986;
+
+-- Select the department number, department name, employee number, last name, and first name of all managers of each department
+
+SELECT
+m.dept_no,
+m.emp_no,
+d.dept_name,
+e.last_name,
+e.first_name
+FROM
+dept_manager m
+INNER JOIN departments d
+ON d.dept_no = m.dept_no
+INNER JOIN employees e
+ON e.emp_no = m.emp_no
+-- Select the employee number, last name, first name, and department of each employee, ordered by employee number
+SELECT
+e.emp_no,
+e.last_name,
+e.first_name,
+d.dept_name
+FROM
+employees e
+LEFT JOIN dept_emp de ON  e.emp_no = de.emp_no
+INNER JOIN
+departments d
+ON
+de.dept_no = d.dept_no
+ORDER BY emp_no
+
+-- Select first name, last name, birth date, and sex of all employees whose first name is "Hercules" and last name begins with a "B"
+SELECT
+first_name,
+last_name,
+birth_date,
+sex
+FROM 
+employees e
+WHERE
+e.first_name ='Hercules' AND (lower(e.last_name) like 'b%')
+
+-- Select employee number, last name, first name, and department name of all employees in the Sales department, ordered by employee number
+SELECT
+* FROM departments
+
+-- Select employee number, last name, first name, and department name of all employees in the 'Sales' OR the 'Development' department, ordered by employee number
+SELECT
+e.emp_no,
+e.last_name,
+e.first_name,
+d.dept_name
+FROM
+employees e
+LEFT JOIN dept_emp de ON  e.emp_no = de.emp_no
+INNER JOIN
+departments d
+ON
+de.dept_no = d.dept_no
+WHERE dept_name = 'Sales' OR dept_name ='Development'
+ORDER BY emp_no
+
+-- Count the number of employees, grouped by last name
+
+SELECT
+last_name,
+COUNT(last_name)
+
+FROM
+employees
+GROUP BY last_name
+
+
+
---
+++ SQL_challenge/schema.sql
@@ -0,0 +1,57 @@
+﻿-- Create the titles table, with a primary id and a title
+
+CREATE TABLE titles(
+"title_id" VARCHAR NOT NULL,
+"title" VARCHAR NOT NULL,
+PRIMARY KEY (title_id))
+
+-- Create an employees table, with the following: 
+CREATE TABLE employees(
+	emp_no INTEGER,
+	emp_title_id VARCHAR NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	sex VARCHAR NOT NULL,
+	hire_date DATE NOT NULL,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id))
+
+
+
+-- Create an departments table, with the following: 
+CREATE TABLE departments(
+	dept_no VARCHAR NOT NULL,
+	dept_name VARCHAR NOT NULL,
+	PRIMARY KEY(dept_no))
+	
+	
+
+-- Create an dept_manager table, with the following: 
+CREATE TABLE dept_manager(
+	dept_no VARCHAR NOT NULL,
+	emp_no INT NOT NULL,
+	PRIMARY KEY (dept_no, emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no))
+	
+
+
+-- Create an dept_emp table, with the following: 
+CREATE TABLE dept_emp(
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	PRIMARY KEY (dept_no, emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no))
+
+
+
+-- Create an salaries table, with the following: 
+CREATE TABLE salaries (
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no))
+	
+
